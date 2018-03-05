1ccd5034e213ef2f78f75702dc59ae24591dbd71
vaidas14 <vaidas.zilenas@gmail.com>
2018-03-05 21:04:45 +0200

    Frontas perkeltas i laravel projekta. Kitas etapas papildu CRUD

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+
+  <section class="">
+    <div class="myvideo">
+      <video data-keepplaying playsinline loop muted class="video" src="/resources/assets/video/intro.mp4" autoplay poster="">
+
+      </video>
+    </div>
+
+  </section>
+
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,71 +1,143 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
+<html>
+  <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+    <title></title>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Bellefair" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  </head>
+    <body>
+      <header>
+        <div class="container">
+          <div class="row">
+            <div class="col-md">
+              <nav>
+                <div class="mobile_lyrika pull-left">
+                  <a href="#"><img src="{{asset('images/img.png')}}" alt="" class="float-md-left lyrika"></a>
                 </div>
+                <button class="navbar-toggler pull-right mobile_icon" id="btn" type="button" data-toggle="collapse" data-target="#collapsingNavbar1" onclick="myFunction()">
+                  <a>   ☰</a>
+                 </button>
+                 <button class="navbar-toggler pull-right second_icon" id="btn" type="button" data-toggle="collapse" data-target="#collapsingNavbar1" onclick="myFunction()">
+                   <a>   ☰</a>
+                  </button>
+                <ul class="nav">
+                  <li class="navigat margin">
+                    <i class="fas fa-home"></i>
+                    <a href="" class="h1a">Home</a>
+                  </li>
+                  <li class="navigat margin">
+                    <i class="fas fa-info"></i>
+                    <a href="" class="h1a">Supplements</a>
+                  </li>
+                  <li class="navigat margin">
+                    <i class="fas fa-users"></i>
+                    <a href="" class="h1a">Coaches</a>
+                  </li>
+                  <div class="dropdown show dropdown_navigat">
+                    <a class="btn h1a dropdown-toggle size" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false" style="font-size: 17px;">
+                      <i class="fas fa-bomb"></i> Workouts
+                    </a>
+
+                    <div class="dropdown-menu text-center" aria-labelledby="dropdownMenuLink">
+                      <a class="dropdown-item" href="#">Crossfit</a>
+                      <a class="dropdown-item" href="#">HIIT</a>
+                      <a class="dropdown-item" href="#">Yoga</a>
+                      <a class="dropdown-item" href="#">Zumba</a>
+                      <a class="dropdown-item" href="#">K-1</a>
+                    </div>
+                  </div>
+                  <li class="navigat margin">
+                    <i class="fas fa-sign-in-alt"></i>
+                    <a href="" class="h1a">Login</a>
+                  </li>
+                  <li class="navigat margin">
+                    <i class="fas fa-registered"></i>
+                    <a href="" class="h1a">Register</a>
+                  </li>
+                  <li class="navigat contact">
+                    <i class="fas fa-address-book"></i>
+                    <a href="" class="h1a">About us</a>
+                  </li>
+                </ul>
+              </nav>
             </div>
-        </nav>
+          </div>
+        </div>
+      </header>
+      <main>
 
-        <main class="py-4">
+
             @yield('content')
-        </main>
-    </div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
+          </main>
+          <footer>
+          <section class="footer_normal">
+            <div class="container">
+              <div class="row">
+                <div class="col-md  text-center">
+                  <div class="contacts">
+                    <i class="far fa-address-card"></i><span> Pranciškonų g. 10, Kaunas</span>
+                  </div>
+                  <div class="contacts">
+                    <i class="fas fa-phone-volume"></i><span> +370 600 44 555</span><br>
+                  </div>
+                  <div class="contacts">
+                    <i class="far fa-envelope"></i><span> lyrika_gym@gmail.com</span>
+                  </div>
+                  <div class="icons">
+                    <a href="#"><i class="fab fa-facebook-square"></i></a>
+                  </div>
+                  <div class="icons">
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                  </div>
+                  <div class="icons">
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                  </div>
+                  <div class="icons">
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </section>
+
+          </footer>
+          <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+          <script type="text/javascript">
+
+
+
+
+
+
+              $(document).ready(function(){
+                    $( ".mobile_icon" ).click(function() {
+                $( ".navigat, .dropdown_navigat" ).toggle( "slow", function() {
+                $( ".mobile_icon" ).hide();
+                $( ".second_icon" ).show();
+              });
+                    });
+
+                    $( ".second_icon" ).click(function() {
+                      $( ".navigat, .dropdown_navigat" ).toggle( "slow", function() {
+                $( ".second_icon" ).hide();
+                $( ".mobile_icon" ).show();
+              });
+                    });
+
+                    });
+
+
+
+
+
+          </script>
+        </body>
+      </html>
